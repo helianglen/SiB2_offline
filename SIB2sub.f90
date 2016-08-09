e1700f4fa3e7556ad29ca0befd9f63cd31e6b38b
Roilan Hdez Valdes <roylanhv@gmail.com>
2016-08-08 21:31:27 -0300

    Learning Git

--- SIB2sub.f90
+++ SIB2sub.f90
@@ -396,8 +396,6 @@ subroutine outer(iout1, nymd)
       vwc01 = vwc01 / wcount01
       vwc02 = vwc02 / wcount02
       
- !********************************************************************   
- 
 ! NEW WAY FOR SIB2DIAG.TXT WRITE  ~RHV
       if (iter == 1) then                                           ! write labels
          write(iout1,'(16(1x,a12))') &
